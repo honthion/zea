60bb37094e46d0b52ae417ca09322a6d81c8f98a
zhangrongxin <zhangrongxin@ibeesaas.com>
2018-11-14 10:15:54 +0800

    add project name in api

--- rauma/urls.py
+++ rauma/urls.py
@@ -34,21 +34,21 @@ router.register(r'users', monitor_views.UserViewSet)
 # The API URLs are now determined automatically by the router.
 # Additionally, we include the login URLs for the browsable API.
 urlpatterns = [
-    url(r'^monitor/', include(monitor_urls)),
+    url(r'^rauma/monitor/', include(monitor_urls)),
 
-    url(r'^register$', monitor_views.register),
-    url(r'^login_out$', monitor_views.logout_view),
-    url(r'^login$', monitor_views.login_index),
-    url(r'^$', monitor_views.login_index),
-    url(r'^index$', monitor_views.index),
-    url(r'^welcome$', monitor_views.welcome, name='welcome'),
+    url(r'^rauma/register$', monitor_views.register),
+    url(r'^rauma/login_out$', monitor_views.logout_view),
+    url(r'^rauma/login$', monitor_views.login_index),
+    url(r'^rauma/$', monitor_views.login_index),
+    url(r'^rauma/index$', monitor_views.index),
+    url(r'^rauma/welcome$', monitor_views.welcome, name='welcome'),
 
-    url(r'^api/', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api-token-auth/', obtain_jwt_token),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^rauma/api/', include(router.urls)),
+    url(r'^rauma/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^rauma/api-token-auth/', obtain_jwt_token),
+    url(r'^rauma/admin/', include(admin.site.urls)),
 
-    url(r'^static/(?P<path>.*)$', static.serve,
+    url(r'^rauma/static/(?P<path>.*)$', static.serve,
         {'document_root': settings.STATIC_ROOT}, name='static')
 ]
 
